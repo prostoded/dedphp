76b1cc7c527fed5850e9940a2f918d688dd76fda
max8web <max-web@ya.ru>
2016-05-13 11:34:35 +0400

    first conflict

--- dedphp.sql
+++ dedphp.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.4.15.5
+-- version 4.0.10
 -- http://www.phpmyadmin.net
 --
--- Хост: 127.0.0.1:3306
--- Время создания: Май 13 2016 г., 08:28
--- Версия сервера: 5.5.48
--- Версия PHP: 5.3.29
+-- Хост: 127.0.0.1:3307
+-- Время создания: Май 13 2016 г., 11:30
+-- Версия сервера: 5.5.38-log
+-- Версия PHP: 5.5.13
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -14,7 +14,7 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
 
 --
 -- База данных: `dedphp`
@@ -23,18 +23,42 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Структура таблицы `categories`
+--
+
+CREATE TABLE IF NOT EXISTS `categories` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` tinytext NOT NULL,
+  `parent_id` int(11) NOT NULL,
+  `showhide` enum('show','hide') NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
+
+--
+-- Дамп данных таблицы `categories`
+--
+
+INSERT INTO `categories` (`id`, `name`, `parent_id`, `showhide`) VALUES
+(1, 'Обувь', 0, 'show'),
+(2, 'Одежда', 0, 'show'),
+(3, 'Аксессуары', 0, 'show');
+
+-- --------------------------------------------------------
+
+--
 -- Структура таблицы `maintexts`
 --
 
 CREATE TABLE IF NOT EXISTS `maintexts` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` tinytext NOT NULL,
   `body` text NOT NULL,
   `url` tinytext NOT NULL,
   `lang` enum('Ru','En','De') NOT NULL DEFAULT 'Ru',
   `showhide` enum('show','hide') NOT NULL DEFAULT 'show',
-  `putdate` date NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+  `putdate` date NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=5 ;
 
 --
 -- Дамп данных таблицы `maintexts`
@@ -46,25 +70,29 @@ INSERT INTO `maintexts` (`id`, `name`, `body`, `url`, `lang`, `showhide`, `putda
 (3, 'О компании', '<p>История компании ASICS началась в 1949 году в послевоенной Японии. Кихатиро Оницука (яп. 鬼塚 喜八郎) задумался, как он может помочь японской молодёжи в такое тяжелое для страны время. Решение пришло быстро, господин Оницука решил создать спортивный бренд для всех, чтобы с помощью спорта воодушевить своих сограждан. Так родилась фирма Onitsuka Tiger.</p><br>\r\n\r\n<p>В первое время своего существования компания поставляла товар только на внутренний рынок Японии, и этим воспользовались основатели корпорации Nike Фил Найт и Билл Бауэрман, закупая кроссовки и перепродавая их в США. В 1977 году после расширения компания Onitsuka Tiger была переименована в ASICS — аббревиатура от «Anima sana in corpore sano», что в переводе с латыни означает «В здоровом теле здоровый дух». </p>', 'about', 'Ru', 'show', '2016-05-11'),
 (4, 'Вакансии', 'Высылайте Ваше резюме на почту re5s@mail.ru', 'vacancy', 'Ru', 'show', '2016-05-11');
 
---
--- Индексы сохранённых таблиц
---
+-- --------------------------------------------------------
 
 --
--- Индексы таблицы `maintexts`
---
-ALTER TABLE `maintexts`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT для сохранённых таблиц
+-- Структура таблицы `products`
 --
 
---
--- AUTO_INCREMENT для таблицы `maintexts`
---
-ALTER TABLE `maintexts`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+CREATE TABLE IF NOT EXISTS `products` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `cat_id` int(11) NOT NULL,
+  `name` tinytext NOT NULL,
+  `body` text NOT NULL,
+  `price` tinytext NOT NULL,
+  `product_code` tinytext NOT NULL,
+  `picture` tinytext NOT NULL,
+  `picture_small` tinytext NOT NULL,
+  `currency` tinytext NOT NULL,
+  `status` tinytext NOT NULL,
+  `user_id` tinytext NOT NULL,
+  `putdate` date NOT NULL,
+  `showhide` enum('show','hide') NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
